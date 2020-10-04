1d91e1e72fc14866ded362f11e680582c4608c1d
esrrhs <esrrhs@163.com>
2020-10-04 17:22:29 +0800

    add

--- app/src/make.bash
+++ app/src/make.bash
@@ -11,10 +11,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 MIN_API=$1
 TARGET=$DIR/bin
 
-try mkdir -p $TARGET/armeabi-v7a $TARGET/x86 $TARGET/arm64-v8a $TARGET/x86_64
+try mkdir -p $TARGET/armeabi-v7a $TARGET/arm64-v8a
 
-export GOPATH=$DIR
-
-pushd $DIR/spp/client
+try mv $DIR/spp/spp_arm_7 $TARGET/armeabi-v7a/libspp.so
+try mv $DIR/spp/spp_android_arm64 $TARGET/arm64-v8a/libspp.so
 
 echo "Successfully build spp"
