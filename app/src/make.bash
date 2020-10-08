9a98dc97e359f36c9b954c470b158430fe8f4294
esrrhs <esrrhs@163.com>
2020-10-08 22:25:44 +0800

    add

--- app/src/make.bash
+++ app/src/make.bash
@@ -10,29 +10,12 @@ TARGET=$DIR/bin
 
 try mkdir -p $TARGET/armeabi-v7a $TARGET/x86 $TARGET/arm64-v8a $TARGET/x86_64
 
-if [ ! -f "$TARGET/armeabi-v7a/libspp.so" ] || [ ! -f "$TARGET/arm64-v8a/libspp.so" ] ||
-   [ ! -f "$TARGET/x86/libspp.so" ] || [ ! -f "$TARGET/x86_64/libspp.so" ]; then
+try cp $DIR/spp/spp-shadowsocks-plugin_android_arm7/spp-shadowsocks-plugin $TARGET/armeabi-v7a/libspp.so
 
-    echo "Cross compile spp for arm"
-    if [ ! -f "$TARGET/armeabi-v7a/libspp.so" ]; then
-        try cp $DIR/spp/spp-shadowsocks-plugin_android_arm7/spp-shadowsocks-plugin $TARGET/armeabi-v7a/libspp.so
-    fi
+try cp $DIR/spp/spp-shadowsocks-plugin_android_arm64/spp-shadowsocks-plugin $TARGET/arm64-v8a/libspp.so
 
-    echo "Cross compile spp for arm64"
-    if [ ! -f "$TARGET/arm64-v8a/libspp.so" ]; then
-        try cp $DIR/spp/spp-shadowsocks-plugin_android_arm64/spp-shadowsocks-plugin $TARGET/arm64-v8a/libspp.so
-    fi
+try cp $DIR/spp/spp-shadowsocks-plugin_android_386/spp-shadowsocks-plugin $TARGET/x86/libspp.so
 
-    echo "Cross compile spp for 386"
-    if [ ! -f "$TARGET/x86/libspp.so" ]; then
-        try cp $DIR/spp/spp-shadowsocks-plugin_android_386/spp-shadowsocks-plugin $TARGET/x86/libspp.so
-    fi
-
-    echo "Cross compile spp for amd64"
-    if [ ! -f "$TARGET/x86_64/libspp.so" ]; then
-        try cp $DIR/spp/spp-shadowsocks-plugin_android_amd64/spp-shadowsocks-plugin $TARGET/x86_64/libspp.so
-    fi
-
-fi
+try cp $DIR/spp/spp-shadowsocks-plugin_android_amd64/spp-shadowsocks-plugin $TARGET/x86_64/libspp.so
 
 echo "Successfully build spp"
