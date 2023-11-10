3a250fe3e009cbd1f7002c974b7250781c588861
esrrhs <esrrhs@163.com>
2023-11-10 10:24:41 +0800

    update

--- sign.sh
+++ sign.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
-/home/project/android/build-tools/29.0.0/apksigner  sign --ks my-release-key.jks --out app-armeabi-v7a-release-signed.apk ./app/build/outputs/apk/release/app-armeabi-v7a-release-unsigned.apk
+/home/project/android/build-tools/34.0.0/apksigner  sign --ks my-release-key.jks --out app-armeabi-v7a-release-signed.apk ./app/build/outputs/apk/release/app-armeabi-v7a-release-unsigned.apk
 
-/home/project/android/build-tools/29.0.0/apksigner  sign --ks my-release-key.jks --out app-arm64-v8a-release-signed.apk ./app/build/outputs/apk/release/app-arm64-v8a-release-unsigned.apk
+/home/project/android/build-tools/34.0.0/apksigner  sign --ks my-release-key.jks --out app-arm64-v8a-release-signed.apk ./app/build/outputs/apk/release/app-arm64-v8a-release-unsigned.apk
 
-/home/project/android/build-tools/29.0.0/apksigner  sign --ks my-release-key.jks --out app-x86_64-release-signed.apk ./app/build/outputs/apk/release/app-x86_64-release-unsigned.apk
+/home/project/android/build-tools/34.0.0/apksigner  sign --ks my-release-key.jks --out app-x86_64-release-signed.apk ./app/build/outputs/apk/release/app-x86_64-release-unsigned.apk
 
-/home/project/android/build-tools/29.0.0/apksigner  sign --ks my-release-key.jks --out app-x86-release-signed.apk ./app/build/outputs/apk/release/app-x86-release-unsigned.apk
+/home/project/android/build-tools/34.0.0/apksigner  sign --ks my-release-key.jks --out app-x86-release-signed.apk ./app/build/outputs/apk/release/app-x86-release-unsigned.apk
 
-/home/project/android/build-tools/29.0.0/apksigner  sign --ks my-release-key.jks --out app-universal-release-signed.apk ./app/build/outputs/apk/release/app-universal-release-unsigned.apk
+/home/project/android/build-tools/34.0.0/apksigner  sign --ks my-release-key.jks --out app-universal-release-signed.apk ./app/build/outputs/apk/release/app-universal-release-unsigned.apk
 
