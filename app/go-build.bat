5cdd19a7ff603eee77e0ea32a82f8820eadbe9b9
esrrhs <esrrhs@163.com>
2024-11-24 12:36:11 +0800

    add

--- app/go-build.bat
+++ app/go-build.bat
@@ -1,49 +1,62 @@
 @echo off
-setlocal
 
-if "%ANDROID_NDK_HOME%"=="" (
-    set ANDROID_NDK_HOME="%ANDROID_HOME%\ndk-bundle"
-)
-
-for /D %%i in (%ANDROID_NDK_HOME%\toolchains\llvm\prebuilt\*) do (
-    set TOOLCHAIN=%%~fi\bin
-    goto :found_toolchain
-)
-
-:found_toolchain
-
-call echo "TOOLCHAIN is %TOOLCHAIN%"
-
-set "ABIS=armeabi-v7a arm64-v8a x86 x86_64"
-set "GO_ARCHS=arm GOARM=7 arm64 386 amd64"
-set "CLANG_ARCHS=armv7a-linux-androideabi aarch64-linux-android i686-linux-android x86_64-linux-android"
-
+echo Building Go app...
+:: from param 1
 set MIN_API=%1
-set ROOT=%~dp0
-set OUT_DIR=%ROOT%build\go
+echo "MIN_API: %MIN_API%"
+echo "ANDROID_HOME: %ANDROID_HOME%"
+echo "ANDROID_NDK_HOME: %ANDROID_NDK_HOME%"
 
-pushd "%ROOT%src\main\go\spp-shadowsocks-plugin"
-set BIN=libspp.so
+set TOOLCHAIN=%ANDROID_NDK_HOME%\toolchains\llvm\prebuilt\windows-x86_64\bin
+echo "TOOLCHAIN: %TOOLCHAIN%"
+
+set ROOT=%~dp0
+echo "ROOT: %ROOT%"
+
+set OUT_DIR=%ROOT%\build\go
+echo "OUT_DIR: %OUT_DIR%"
+
+cd %ROOT%\src\main\go\spp-shadowsocks-plugin
+echo "Building spp-shadowsocks-plugin..."
 
 go mod tidy
 
-for %%a in (%ABIS%) do (
-    set ABI=%%a
-    call echo Building %BIN% %%a
-    mkdir "%OUT_DIR%\%%a"
-    
-    call set "GOARCH=%%a"
-    
-    %TOOLCHAIN%\%CLANG_ARCHS:~%%~a=%%~a% %MIN_API%-clang
-    set "GOOS=android"
-    
-    go build -v -ldflags="-s -w" -o "%OUT_DIR%\unstripped"
-    
-    if errorlevel 1 exit /b -1
+:: CLANG_ARCHS: armv7a-linux-androideabi aarch64-linux-android i686-linux-android x86_64-linux-android
+:: GO_ARCHS: 'arm GOARM=7' arm64 386 amd64
 
-    %TOOLCHAIN%\llvm-strip "%OUT_DIR%\unstripped" -o "%OUT_DIR%\%%a\%BIN%"
-    del "%OUT_DIR%\unstripped"
-)
+set CGO_ENABLED=1
+set GOOS=android
 
-popd
-endlocal
+:: armv7a-linux-androideabi
+set CC=%TOOLCHAIN%\armv7a-linux-androideabi%MIN_API%-clang
+set GOARCH=arm
+set GOARM=7
+go build -v -ldflags="-s -w" -o "%OUT_DIR%/unstripped"
+%TOOLCHAIN%\llvm-strip "%OUT_DIR%/unstripped" -o "%OUT_DIR%\armeabi-v7a\libspp.so"
+del "%OUT_DIR%\unstripped"
+echo "armv7a-linux-androideabi done"
+
+:: aarch64-linux-android
+set CC=%TOOLCHAIN%\aarch64-linux-android%MIN_API%-clang
+set GOARCH=arm64
+go build -v -ldflags="-s -w" -o "%OUT_DIR%/unstripped"
+%TOOLCHAIN%\llvm-strip "%OUT_DIR%/unstripped" -o "%OUT_DIR%\arm64-v8a\libspp.so"
+del "%OUT_DIR%\unstripped"
+echo "aarch64-linux-android done"
+
+:: i686-linux-android
+set CC=%TOOLCHAIN%\i686-linux-android%MIN_API%-clang
+set GOARCH=386
+go build -v -ldflags="-s -w" -o "%OUT_DIR%/unstripped"
+%TOOLCHAIN%\llvm-strip "%OUT_DIR%/unstripped" -o "%OUT_DIR%\x86\libspp.so"
+del "%OUT_DIR%\unstripped"
+echo "i686-linux-android done"
+
+:: x86_64-linux-android
+set CC=%TOOLCHAIN%\x86_64-linux-android%MIN_API%-clang
+set GOARCH=amd64
+go build -v -ldflags="-s -w" -o "%OUT_DIR%/unstripped"
+%TOOLCHAIN%\llvm-strip "%OUT_DIR%/unstripped" -o "%OUT_DIR%\x86_64\libspp.so"
+del "%OUT_DIR%\unstripped"
+echo "x86_64-linux-android done"
+
